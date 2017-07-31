7b3a6ad9372634e56f1159d80c8c6a575ae90bb9
Oliver Friedrich <olifriedrich@gmail.com>
2017-08-01 00:42:12 +0200

    Deleted rdflib examples

--- dk_example.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-url = "http://dbpedia.org/resource/Category:Municipal_seats_of Denmark"
-seats = requests.get(url, headers={'Accept': 'application/json'}).json()
-
-print(seats)
-
